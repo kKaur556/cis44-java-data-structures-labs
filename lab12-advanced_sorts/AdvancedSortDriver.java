9f89b1ad073fec4816fd1c09fa32890a9027debb
kKaur556 <cookie2229@icloud.com>
2025-11-20 20:19:45 -0800

    Update AdvancedSortDriver.java

--- lab12-advanced_sorts/AdvancedSortDriver.java
+++ lab12-advanced_sorts/AdvancedSortDriver.java
@@ -5,15 +5,37 @@ import java.util.Random;
 public class AdvancedSortDriver {
     public static void main(String[] args) {
         Comparator<Integer> comp = Comparator.naturalOrder();
-        int N = 100;
+        int N = 20;
         Random rand = new Random();
 
         Integer[] arr1 = new Integer[N];
         for (int i = 0; i < N; i++) {
-            arr1[i] = rand.nextInt(1000);
+            arr1[i] = rand.nextInt(100);
         }
         Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
 
+        System.out.println("--- Test: Random Array (N=20) ---");
+        System.out.println("Original: " + Arrays.toString(arr1));
+
+        AdvancedSorters.mergeSort(arr1, comp);
+        System.out.println("Merge Sort: " + Arrays.toString(arr1));
+
+        System.out.println("Original: " + Arrays.toString(arr2));
+        AdvancedSorters.quickSort(arr2, comp);
+        System.out.println("Quick Sort: " + Arrays.toString(arr2));
+        
+        
+        System.out.println();
+        
+        //N= 100
+        N = 100;
+        arr1 = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            arr1[i] = rand.nextInt(100);
+        }
+        
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        
         System.out.println("--- Test: Random Array (N=100) ---");
         System.out.println("Original: " + Arrays.toString(arr1));
 
@@ -23,5 +45,6 @@ public class AdvancedSortDriver {
         System.out.println("Original: " + Arrays.toString(arr2));
         AdvancedSorters.quickSort(arr2, comp);
         System.out.println("Quick Sort: " + Arrays.toString(arr2));
+        
     }
 }
