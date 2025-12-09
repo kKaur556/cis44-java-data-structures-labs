eac8e5713da651ebe1a661004537968c34400e59
kKaur556 <cookie2229@icloud.com>
2025-12-09 15:06:16 -0800

    Update ProjectTester.java

--- Exam3-Capstone_Project/ProjectTester.java
+++ Exam3-Capstone_Project/ProjectTester.java
@@ -12,10 +12,10 @@ public class ProjectTester {
         
         /* expected behavior:
         cheapest is sunol-sunol: $0.00
-        sunol- livermore: $4.00
-        sunol- pleasanton: $3.00
-        sunol- newark: $10.00
-        
+        Sunol: $0.00
+        Livermore: $4.00
+        Pleasanton: $3.00
+        Newark: $10.00
         */
         
         opt.findCheapestRoute(graph1, "Sunol");
@@ -28,6 +28,15 @@ public class ProjectTester {
         graph2.addEdge(new Edge("San Francisco", "Roseville", 20.0));
         graph2.addEdge(new Edge("San Jose", "Santa Clara", 1.0));
         
+        /* expected behavior:
+        santa clara and san jose unreachable- infinity
+        San Francisco: $15.00
+        Santa Clara: Infinity
+        Sacramento: $0.00
+        Roseville: $35.00
+        San Jose: Infinity
+        */
+        
         opt.findCheapestRoute(graph2, "Sacramento");
         System.out.println();
         
@@ -39,7 +48,11 @@ public class ProjectTester {
         graph3.addEdge(new Edge("Tracy", "Davis", -8.0));
         graph3.addEdge(new Edge("Davis", "Patterson", -5.0));
         
+        /* expected behavior:
+        prints: Graph contains a negative cost cycle! Incosistent pricing.
+        */
         opt.findCheapestRoute(graph3, "Patterson");
+        
         System.out.println();
     }
 }
